1a6dae194f6c33813ab92c93e7ba51f96456e8d7
Enotakin <just.enotakin@gmail.com>
2023-03-16 06:44:42 +0100

    Plugin base implemented

---
+++ src/main/java/com/nftworlds/gradients/NFTWGradientsPlugin.java
@@ -0,0 +1,12 @@
+package com.nftworlds.gradients;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NFTWGradientsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
